76a6a712a9be1262f2763241905c1170dca59aa7
M1KZER <102239415+thamanee@users.noreply.github.com>
2025-02-05 05:57:14 +0700

    EditPlayer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -61,8 +61,8 @@ public class PlayerController : MonoBehaviour
         
         if (moveInput != 0)
         {
-            //transform.localScale = new Vector3(Mathf.Sign(moveInput), 0.1f, 0.1f);
-            transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            transform.localScale = new Vector3(Mathf.Sign(moveInput) * 0.1f, 0.1f, 0.1f);
+            //transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
             anim.SetBool("isRunning", true);
         }
         else
